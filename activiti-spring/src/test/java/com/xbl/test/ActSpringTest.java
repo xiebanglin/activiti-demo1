926908e8c3529189274c0f784c8cdbbccdf90cd5
xiebanglin <2419507265@qq.com>
2021-02-05 22:24:35 +0800

    整合spring、整合springboot

---
+++ activiti-spring/src/test/java/com/xbl/test/ActSpringTest.java
@@ -0,0 +1,33 @@
+package com.xbl.test;
+
+import org.activiti.engine.HistoryService;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:activiti-spring.xml")
+public class ActSpringTest {
+
+    @Autowired
+    private RepositoryService repositoryService;
+
+    @Autowired
+    private RuntimeService runtimeService;
+
+    @Autowired
+    private TaskService taskService;
+
+    @Autowired
+    private HistoryService historyService;
+
+    @Test
+    public void testRep() {
+        System.out.println(repositoryService);
+    }
+}
